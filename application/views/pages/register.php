7d216ed4197da26aa9ff31b397eaa8a65ce0a095
Michael Stevens <mich8060@hotmail.com>
2014-08-07 11:55:43 -0600

    Zip Google API integration

--- application/views/pages/register.php
+++ application/views/pages/register.php
@@ -6,27 +6,33 @@
 					<li class="active">
 						<div class="rows">
 							<label data="We won't send you spam, we promise...">What's your email address?</label>
-							<input type="email" name="email" id="email" placeholder="Email Address" data="Please enter a valid email address" />
+							<input type="email" name="email" id="email" placeholder="Email Address" />
+							<div class="error">Please enter a valid email address</div>
 						</div>
 						<a href="#" class="button black">Continue</a> <span class="hint">OR PRESS ENTER</span>
 					</li>
 					<li>
 						<div class="rows">
-							<label for="name">What's your full name?</label>
-							<input type="text" name="name" id="name" placeholder="John Doe" data="Please enter a your full name" />
+							<label data="So people know what to call you" for="name">What's your full name?</label>
+							<input type="text" name="name" id="name" placeholder="John Doe" />
+							<div class="error">Please enter your full name</div>
 						</div>
 						<a href="#" class="button black">Continue</a> <span class="hint">OR PRESS ENTER</span>
 					</li>
 					<li>
 						<div class="rows">
-							<label for="name">Select a username?</label>
-							<input type="text" name="name" id="name" placeholder="Password" />
+							<label data="A unique identification for you" for="display">Select a username?</label>
+							<input type="text" name="display" id="display" placeholder="Username" autocomplete="off" />
+							<div class="clue">
+								Your iSnap URL: http://isnap.me/<span class="display"></span>
+								<span class="status"></span>
+							</div>
 						</div>
 						<a href="#" class="button black">Continue</a> <span class="hint">OR PRESS ENTER</span>
 					</li>
 					<li>
 						<div class="rows">
-							<label for="name">Select a password?</label>
+							<label data="A secret word or phrase that only you know" for="name">Select a password?</label>
 							<input type="password" name="name" id="name" placeholder="Password" />
 						</div>
 						<a href="#" class="button black">Continue</a> <span class="hint">OR PRESS ENTER</span>
@@ -34,22 +40,27 @@
 					<li>
 						<div class="rows">
 							<label>What's your gender?</label>
-							<input type="radio" name="gender" /> Male
-							<input type="radio" name="gender" /> Female
+							<div>
+								<label for="male">Male</label>
+								<input type="radio" name="gender" id="male" />
+								<label for="female">Female</label>
+								<input type="radio" name="gender" id="female" /> 
+							</div>
 						</div>
 						<a href="#" class="button black">Continue</a> <span class="hint">OR PRESS ENTER</span>
 					</li>
 					<li>
 						<div class="rows">
 							<label>What year were you born?</label>
-							<input type="number" value="1982" min="1920" max="2014" />
+							<input type="number" placeholder="1982" min="1920" max="2014" />
 						</div>
 						<a href="#" class="button black">Continue</a> <span class="hint">OR PRESS ENTER</span>
 					</li>
 					<li>
 						<div class="rows">
 							<label>What's your 5 digit zip code</label>
-							<input type="number" value="90210" min="0" max="99999" />
+							<input type="number" placeholder="90210" id="zipcode" name="zipcode" min="0" max="99999" />
+							<div class="clue"></div>
 						</div>
 						<a href="#" class="button black">Continue</a> <span class="hint">OR PRESS ENTER</span>
 					</li>
